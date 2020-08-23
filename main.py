e08a230b0e324ad2440e82d664ee971c99ac79e2
Sahil Appayev <sahilappayev@gmail.com>
2020-08-23 23:37:02 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,181 @@
+import student
+
+# main functions section
+def addNewStudent():
+    def setStudentNo():
+        studentNo_ = input("3 reqemden ibaret telebe nomresini daxil edin: ").strip()
+        if studentNo_.isdigit() and len(studentNo_) == 3:
+            for s in student.userList:
+                if s.studentNo == studentNo_:
+                    print("Bu koda sahib telebe artiq sistemde var!")
+                    break
+            return studentNo_
+
+        else:
+            print("Telebe nomresi dogru daxil edilmeyib!")
+            showMenu()
+    studentNo = setStudentNo()
+
+    def setName():
+        return input("Adi daxil edin: ").strip()
+    name = setName()
+
+    def setSurname():
+        return input("Soyadi daxil edin: ").strip()
+    surname = setSurname()
+
+    def setEmail():
+        email_ = input("Emaili daxil edin: ").strip()
+        if email_.find("@") != -1:
+            return email_
+        else:
+            print("Email melumatlari dogru daxil edilmeyib! Tekarar daxil edin.")
+            showMenu()
+    email = setEmail()
+
+    def setPhone():
+        phone_ = input("Telefon nomresini daxil edin (+994_________): ").strip()
+        if phone_.startswith("+994") and len(phone_) == 13:
+            return phone_
+        else:
+            print("Telefon nomresi dogru daxil edilmeyib! Tekrar daxil edin.")
+            showMenu()
+    phone = setPhone()
+
+    obj = student.Student(studentNo, name, surname, email, phone)
+    student.userList.append(obj)
+
+def printStudents():
+    if len(student.userList) == 0:
+        print("Sistemde telebe yoxdur!")
+    else:
+        for s in student.userList:
+            s.printInfo()
+
+def deleteStudent():
+    studentNo_ = input("Silinecek telebenin kodunu daxil edin:").strip()
+    if studentNo_.isdigit() and len(studentNo_) == 3:
+        check = False
+
+        def checkStudents():
+            for x in student.userList:
+                if x.studentNo == studentNo_:
+                    return True
+
+        check = checkStudents()
+        if check:
+            for s in student.userList:
+                if studentNo_ == s.studentNo:
+                    student.userList.remove(s)
+        else:
+            print("Sistemde bu koda sahib telebe tapilmadi!")
+
+    else:
+        print("Telebe nomresi dogru daxil edilmeyib!")
+
+def editStudent():
+    studentNo = input("Melumatlarina duzelish edilecek telebenin kodunu daxil edin:").strip()
+
+    if studentNo.isdigit() and len(studentNo) == 3:
+        check = False
+
+        def checkStudents():
+            for x in student.userList:
+                if x.studentNo == studentNo:
+                    return True
+
+        check = checkStudents()
+        if check:
+            for s in student.userList:
+                if s.studentNo == studentNo:
+                    name = input("Adi daxil edin: ").strip()
+
+                    surname = input("Soyadi daxil edin: ").strip()
+
+                    def setEmail():
+                        email_ = input("Emaili daxil edin: ").strip()
+                        if email_.find("@") != -1:
+                            return email_
+                        else:
+                            print("Email melumatlari dogru daxil edilmeyib! Tekarar daxil edin.")
+                            setEmail()
+
+                    email = setEmail()
+
+                    def setPhone():
+                        phone_ = input("Telefon nomresini daxil edin (+994_________): ").strip()
+                        if phone_.startswith("+994") and len(phone_) == 13:
+                            return phone_
+                        else:
+                            print("Telefon nomresi dogru daxil edilmeyib! Tekrar daxil edin.")
+                            setPhone()
+
+                    phone = setPhone()
+
+                    s.name = name
+                    s.surname = surname
+                    s.email = email
+                    s.phone = phone
+
+        else:
+            print("Sistemde bu koda sahib telebe tapilmadi!")
+    else:
+        print("Telebe nomresi dogru daxil edilmeyib! Tekrar daxil edin.")
+
+def printByName():
+    name = input("Telebe adini daxil edin: ").strip()
+    check = False
+
+    def checkStudents():
+        for x in student.userList:
+            if name == x.name:
+                return True
+
+    check = checkStudents()
+
+    if check:
+        for s in student.userList:
+            if name == s.name:
+                s.printInfo()
+    else:
+        print("Sistemde bu ada sahib telebe tapilmadi!")
+
+
+# menu section
+def menu():
+    return int(input("""
+        1. Telebe daxil et
+        2. Telebe koduna gore telebe melumatlarini sil
+        3. Telebe koduna gore telebe melumatlarini deyishdir
+        4. Telebe adina gore telebe melumatlarini goster
+        5. Butun telebelerin melumatlarini goster
+        6. Sistemden chix
+
+        Menyudaki emrleri icra etmek uhun muvafiq reqemi daxil edin:
+    """).strip())
+
+def showMenu():
+    command = menu()
+
+    if command == 1:
+        if len(student.userList) < 10:
+            print(len(student.userList))
+            addNewStudent()
+        else:
+            print("Sistemde yeni telebe uchun yer yoxdur! Maksimum 10 telebe daxil ede bilersiz.")
+    elif command == 2:
+        deleteStudent()
+    elif command == 3:
+        editStudent()
+    elif command == 4:
+        printByName()
+    elif command == 5:
+        printStudents()
+    elif command == 6:
+        exit()
+    else:
+        print("Daxil etdiyiniz emr dogru deyil!")
+
+# App start section
+while True:
+    showMenu()
---
+++ student.py
@@ -0,0 +1,29 @@
+class Student:
+    studentNo = None
+    name = None
+    surname = None
+    email = None
+    phone = None
+
+
+
+    def __init__(self, _studentNo, _name, _surname, _email, _phone):
+        self.studentNo = _studentNo
+        self.name = _name
+        self.surname = _surname
+        self.email = _email
+        self.phone = _phone
+
+
+    def printInfo(self):
+        print(
+            f"""
+            Telebe kodu: {self.studentNo}
+            Ad: {self.name}
+            Soyad: {self.surname}
+            Email: {self.email}
+            Telefon: {self.phone}
+            """
+        )
+
+userList = []
